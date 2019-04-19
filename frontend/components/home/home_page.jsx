233799962ed22f548052459b3f1c72ae6de42b08
Bryan Lynch <bml312@nyu.edu>
2019-04-19 15:29:00 -0400

    update map + background color

--- frontend/components/home/home_page.jsx
+++ frontend/components/home/home_page.jsx
@@ -9,6 +9,6 @@ export default() => (
             <SearchBar />
             {/* Reviews */}
             {/* Bed Info */}
-            {/* <Map /> */}
+            <Map />
     </div>
 );
--- frontend/components/map/map.jsx
+++ frontend/components/map/map.jsx
@@ -1,32 +1,9 @@
 import React from 'react';
 
-class Map extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+export default() => (
+    <div className="map-section">
+        <h1>Our Location</h1>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.643551080697!2d121.51356201412109!3d25.046167983966615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a9731dc935bf%3A0x385a1d30e5dab0a0!2zTUFOR08gU1RBVElPTiBIT1NURUzoipLmnpzpqZvnq5nml4Xlupc!5e0!3m2!1sen!2sus!4v1555700806681!5m2!1sen!2sus" width="800" height="600" frameBorder="0" style={{border: 0}}></iframe>
+    </div>
+)
 
-    componentDidMount() {
-        const mapOptions = {
-            center: { lat: 25.046168, lng: 121.5157507 }, 
-            zoom: 15
-        };
-
-        this.map = new google.maps.Map(this.mapNode, mapOptions);
-        new google.maps.Marker({
-            position: mapOptions.center,
-            map: this.map,
-        });
-    }
-
-    render() {
-        return (
-            <div className="map-section">
-                <h1>Our Location</h1>
-                <div id="map-container" ref={map => this.mapNode = map}>
-                </div>
-            </div>
-        )
-    }
-}
-
-export default Map;
