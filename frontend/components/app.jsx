cd297b13df3e9d85d6ddbf77aa4279c8f0fbec82
Bryan Lynch <bml312@nyu.edu>
2019-04-20 18:06:07 -0400

    update dropdown menu change bg cover

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -22,11 +22,7 @@ export default () => (
       <AuthRoute exact path="/admin" component={LoginFormContainer} />
       {/* <AuthRoute exact path="/signup" component={SignupFormContainer} /> 
       {/* <ProtectedRoute exact path="/admin" component={AdminPageContainer} /> */}
-      {/* <Route path="/about" component={About} /> */}
-      {/* <Route path="/amenities" component={BookingIndexContainer} />
-      <Route path="/payment" component={BookingIndexContainer} />
-      <Route path="/contact" component={BookingIndexContainer} />  */}
-
+      <Route path="/booking" component={BookingIndexContainer} />
       <Route path="/confirmation" component={ConfirmationContainer} />
     </Switch>
   </div>
--- frontend/components/home/popups/amenities.jsx
+++ frontend/components/home/popups/amenities.jsx
@@ -5,11 +5,20 @@ export default () => (
     <h1>Amenities</h1>
     <ul>
       The following amenities are included during your stay:
-      <li>Shower</li> 
-      <li>Lockers</li> 
-      <li>Washer</li> 
-      <li>Towels</li> 
-      <li>Free Bag Check</li> 
+      <li>Shower</li>
+      <li>Lockers</li>
+      <li>Washer</li>
+      <li>Towels</li>
+      <li>Free Bag Check</li>
+      <li>Late Checkout if Necessary</li>
+    </ul>
+    <ul>
+      Mango Station Hostel is located near the following attractions:
+      <li>Shin Kong Life Tower 1 min</li>
+      <li>Taipei Main Station 3 min</li>
+      <li>National Taiwan Museum 5 min</li>
+      <li>Taipei Zhongshan Hall 10-15 min</li>
+      <li>National Taiwan University 20 min (by subway)</li>
     </ul>
   </div>
 );
