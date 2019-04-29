00cd77e348a85cf0f173642c562e6f7c4f7c7527
Bryan Lynch <bml312@nyu.edu>
2019-04-29 17:30:06 -0400

    add sessions unit test

--- spec/controllers/api/sessions_controller_spec.rb
+++ spec/controllers/api/sessions_controller_spec.rb
@@ -20,4 +20,24 @@ RSpec.describe Api::SessionsController, type: :controller do
       end
     end
   end
+
+  describe "POST #destroy" do
+    context "current user is nil" do
+      it "does not logout a user" do
+        post 'create', params: { user: { username: 'p123'} }, format: :json
+        expect(response).not_to render_template('api/users/show')
+        expect(response).to have_http_status(422)
+      end
+    end
+
+    context "with valid params" do
+      it "goes to user show page on success" do
+        test_user = User.create(username: "plato123", password: "socrates123",
+        email: "plato@mail.com")
+        post 'create', params: {user: {username: "plato123", password: "socrates123"}}, format: :json
+        post 'destroy', format: :json
+        expect(response[:current_user]).to be_nil
+      end
+    end
+  end
 end
