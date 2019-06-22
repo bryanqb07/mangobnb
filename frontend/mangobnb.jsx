9ea7f66ab0a319d430ce09d05dc959e8572194ca
Bryan Lynch <bml312@nyu.edu>
2019-06-22 21:08:53 +0900

    test

--- frontend/mangobnb.jsx
+++ frontend/mangobnb.jsx
@@ -5,7 +5,6 @@ import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", ()=> {
     const root = document.getElementById("root");
-
     let store;
     if(window.currentUser){
       const preLoadedState = {
